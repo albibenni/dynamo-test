421358671c11e782aa94ba224a6692df02c05848
Alberto Benatti <albi.benni8@gmail.com>
2024-07-22 13:59:28 +0200

    Create update-book.test.ts

---
+++ __tests__/books/update-book.test.ts
@@ -0,0 +1,61 @@
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { PutCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { beforeAll, expect, describe, it } from "vitest";
+import { z } from "zod";
+
+const bookSchema = z.object({
+    pk: z.string(),
+    sk: z.string(),
+    author: z.string(),
+    title: z.string(),
+});
+
+// NOTE: Using the local endpoint for DynamoDB to connect to our Docker container
+const dynamodb = new DynamoDB({
+    endpoint: "http://localhost:8000",
+});
+const TableName = "test-db";
+
+describe("update-book", () => {
+    beforeAll(async () => {
+        await dynamodb.send(
+            new PutCommand({
+                TableName,
+                Item: {
+                    pk: "USER#2",
+                    sk: "BOOK#2",
+                    author: "Example Author",
+                    title: "Example Title",
+                },
+            }),
+        );
+    });
+
+    describe("SUCCESS", () => {
+        it("updates a book", async () => {
+            const { Attributes: book } = await dynamodb.send(
+                new UpdateCommand({
+                    TableName,
+                    Key: {
+                        pk: "USER#2",
+                        sk: "BOOK#2",
+                    },
+                    UpdateExpression: "set author = :a",
+                    ExpressionAttributeValues: {
+                        ":a": "Author 2",
+                    },
+                    ReturnValues: "ALL_NEW",
+                }),
+            );
+
+            const parsedBook = bookSchema.parse(book);
+
+            expect(parsedBook).toMatchObject({
+                pk: "USER#2",
+                sk: "BOOK#2",
+                author: "Author 2",
+                title: "Example Title",
+            });
+        });
+    });
+});
