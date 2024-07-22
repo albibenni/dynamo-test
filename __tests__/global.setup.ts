28b5788f1d81e7e7e67ecec7f83b86fdfce950eb
Alberto Benatti <albi.benni8@gmail.com>
2024-07-22 13:59:27 +0200

    Create global.setup.ts

---
+++ __tests__/global.setup.ts
@@ -0,0 +1,90 @@
+import {
+    CreateTableCommand,
+    DeleteTableCommand,
+    DescribeTableCommand,
+    DynamoDB,
+    ListTablesCommand,
+    TableStatus,
+} from "@aws-sdk/client-dynamodb";
+
+// NOTE: Using the local endpoint for DynamoDB to connect to our Docker container
+const dynamodb = new DynamoDB({
+    endpoint: "http://localhost:8000",
+});
+const TableName = "test-db";
+
+async function pollTablesList(tableName: string) {
+    // Fetch a list of the current tables in the database
+    const tables = await dynamodb.send(new ListTablesCommand());
+
+    // If the current tables includes our table name, keep checking
+    if (tables.TableNames && tables.TableNames.includes(tableName)) {
+        await pollTablesList(tableName);
+    }
+}
+
+async function pollTable(status: TableStatus) {
+    // Fetch the current table from the database
+    const table = await dynamodb.send(
+        new DescribeTableCommand({
+            TableName,
+        }),
+    );
+
+    // If the current status is not the provided status ("ACTIVE") then check again
+    if (table.Table && table.Table.TableStatus !== status) {
+        await pollTable(status);
+    }
+}
+
+export async function setup() {
+    console.log("Running Setup");
+    try {
+        // When setting up the tests, create a new table on our docker database
+        await dynamodb.send(
+            new CreateTableCommand({
+                TableName,
+                AttributeDefinitions: [
+                    {
+                        AttributeName: "pk",
+                        AttributeType: "S",
+                    },
+                    {
+                        AttributeName: "sk",
+                        AttributeType: "S",
+                    },
+                ],
+                KeySchema: [
+                    {
+                        AttributeName: "pk",
+                        KeyType: "HASH",
+                    },
+                    {
+                        AttributeName: "sk",
+                        KeyType: "RANGE",
+                    },
+                ],
+                BillingMode: "PAY_PER_REQUEST",
+            }),
+        );
+
+        // Poll the table status to ensure it's ACTIVE before allowing tests to run
+        await pollTable("ACTIVE");
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export async function teardown() {
+    console.log("Running Teardown");
+
+    // Delete the table we created in the setup function
+    await dynamodb.send(
+        new DeleteTableCommand({
+            TableName,
+        }),
+    );
+
+    // Poll the table to ensure it's been deleted before exitting
+    await pollTablesList(TableName);
+}
